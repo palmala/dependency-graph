f50625c358fb414d174ae7f84781b220874c502f
Laszlo Palmai <palmala@users.noreply.github.com>
2024-03-30 21:19:02 +0100

    Fixing pom_reader

--- pom_reader.py
+++ pom_reader.py
@@ -1,18 +1,22 @@
 import xml.etree.ElementTree as ET
+import re
+
+
+def namespace(element):
+    m = re.match(r'\{.*\}', element.tag)
+    return m.group(0) if m else ''
 
 
 def main():
-    tree = ET.parse('example.pom')
-    root = tree.getroot()
-    dependencies = root.findall('./dependency')
-    if dependencies:
-        for dependency in root.findall('./dependency'):
-            group_id = dependency.find('groupId').text
-            artifact_id = dependency.find('artifactId').text
-            version = dependency.find('version').text
-            print(group_id, artifact_id,version)
-    else:
-        print("Nope!")
+    root = ET.parse('example.pom')
+    ns = namespace(root.getroot())
+    dependencies = root.find(ns + 'dependencies')
+    for dependency in dependencies:
+        group_id = dependency.find(ns + 'groupId').text
+        artifact_id = dependency.find(ns + 'artifactId').text
+        version = dependency.find(ns + 'version').text
+        print(group_id, artifact_id, version)
+
 
 if __name__ == "__main__":
     main()
